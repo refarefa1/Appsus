724aae6421a59a86e8eb62d53711148e02c2d71c
varditomer <tomer.vardi@gmail.com>
2022-11-09 18:43:27 +0200

    note edit

--- apps/keep/pages/keep-app.cmp.js
+++ apps/keep/pages/keep-app.cmp.js
@@ -2,6 +2,7 @@ import noteService from '../services/note.service.js'
 
 import noteList from '../cmps/note-list.cmp.js'
 import noteAdd from '../cmps/note-add.cmp.js'
+import noteEdit from '../cmps/note-edit.cmp.js'
 
 export default {
     name: `keep-app`,
@@ -9,11 +10,13 @@ export default {
     template: `
     <section className="keep-app">
         <h1>keep-app</h1>
+        <note-edit v-if="noteToEdit" :note="noteToEdit" @noteEdited="edit"/>
 
         <note-add @noteSaved="save" />
 
         <note-list v-if="notes"
             @remove="removeNote" 
+            @noteClicked="noteClicked"
             :notes="notes" />
     </section>;
   
@@ -26,6 +29,7 @@ export default {
     data() {
         return {
             notes: null,
+            noteToEdit: null
         }
     },
     methods: {
@@ -43,14 +47,27 @@ export default {
                 })
         },
         save(note) {
-            console.log(`note:`, note)
             this.notes.push(note)
+        },
+        edit(note) {
+            console.log(`note:`, note)
+            noteService.edit(note)
+                .then(note => {
+                    console.log(`note:`, note)
+                    this.noteToEdit = null
+                })
+        },
+        noteClicked(note) {
+            console.log(`note clicked and arrived to daddy:`, note)
+            this.noteToEdit = note
         }
+
     },
     computed: {},
     components: {
         noteList,
         noteAdd,
+        noteEdit,
 
     }
 }
