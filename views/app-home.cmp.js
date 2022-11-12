8fedf31ccf9db14b6e2b2b21a382f0841acdb932
Refael Abramov <refaelavramov@gmail.com>
2022-11-12 16:36:05 +0200

    homepage

--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -4,8 +4,16 @@ export default {
             <img class="home-hero" src="assets/img/hero.jpg" alt="hero" />
             <h1 class="home-title">Welcome to a new world of Technology</h1>
             <section className="home-btns">
+
+                <router-link to="/email/inbox">
                 <button>Mail</button>
+                </router-link>
+
+                <router-link to="/keep">
                 <button>Keep</button>
+                </router-link>
+
+
             </section>
         </section>
     `,
